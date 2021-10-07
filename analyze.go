4edc78872fc6c3595a8c7f564c9f571090bfa8c8
Lucas Charles <me@lucascharles.me>
2021-10-07 08:54:12 -0700

    chore: Use ruleset.ProcessPassthrough
    DRY up passthrough parsing by relying on ruleset library directly
    
    Relates to https://gitlab.com/gitlab-org/gitlab/-/issues/342434

--- analyze.go
+++ analyze.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"fmt"
 	"io"
-	"io/ioutil"
 	"os"
 	"os/exec"
 	"path"
@@ -125,7 +123,7 @@ func buildArgs(configPath, outputPath, projectPath, excludedPaths string, enable
 func getConfigPath(projectPath string, rulesetConfig *ruleset.Config) (string, error) {
 	if rulesetConfig != nil && len(rulesetConfig.PassThrough) != 0 {
 		passThrough := rulesetConfig.PassThrough[0]
-		configPath, err := processPassthrough(projectPath, passThrough)
+		configPath, err := ruleset.ProcessPassthrough(projectPath, passThrough, log.StandardLogger())
 
 		if err != nil {
 			return "", err
@@ -135,76 +133,4 @@ func getConfigPath(projectPath string, rulesetConfig *ruleset.Config) (string, e
 	}
 
 	return path.Join("/", "rules"), nil
-}
-
-// processPassthrough processes the ruleset and returns the set of args to appended
-// to the analyzer command
-func processPassthrough(projectPath string, passThrough ruleset.PassThrough) (string, error) {
-	value := passThrough.Value
-	if passThrough.Type == ruleset.PassThroughFile {
-		return passthroughFile(projectPath, value)
-	} else if passThrough.Type == ruleset.PassThroughRaw {
-		return passthroughRaw(value)
-	} else {
-		return "", fmt.Errorf("unsupported passthrough type: %s", passThrough.Type)
-	}
-}
-
-// passthroughFile will check to make sure file/dir exists
-func passthroughFile(projectPath string, filename string) (string, error) {
-	configPath := filepath.Join(projectPath, cleanPath(filename))
-	if _, err := os.Stat(configPath); err != nil {
-		return "", err
-	}
-
-	log.Infof("Using config from custom ruleset passthrough file: %s\n", filename)
-	return configPath, nil
-}
-
-// passthroughRaw writes raw configuration to a file
-func passthroughRaw(raw string) (string, error) {
-	// create tmp config for raw values from the passthrough to be loaded into
-	tmpConfig, err := ioutil.TempFile("", "rules*.yml")
-	if err != nil {
-		return "", fmt.Errorf("failed to create tmp semgrep rules file")
-	}
-
-	if _, err = tmpConfig.Write([]byte(raw)); err != nil {
-		return "", fmt.Errorf("failed to write to create tmp semgrep rules file")
-	}
-	log.Info("Loading config from custom ruleset via raw passthrough")
-
-	return tmpConfig.Name(), nil
-}
-
-// CleanPath makes a path safe for use with filepath.Join. This is done by not
-// only cleaning the path, but also (if the path is relative) adding a leading
-// '/' and cleaning it (then removing the leading '/'). This ensures that a
-// path resulting from prepending another path will always resolve to lexically
-// be a subdirectory of the prefixed path. This is all done lexically, so paths
-// that include symlinks won't be safe as a result of using CleanPath.
-//
-// This function comes from runC (libcontainer/utils/utils.go):
-// https://github.com/opencontainers/runc/blob/d636ad6256f9194b0f4c6ee181e75fb36e3446d8/libcontainer/utils/utils.go#L53
-func cleanPath(path string) string {
-	// Deal with empty strings nicely.
-	if path == "" {
-		return ""
-	}
-
-	// Ensure that all paths are cleaned (especially problematic ones like
-	// "/../../../../../" which can cause lots of issues).
-	path = filepath.Clean(path)
-
-	// If the path isn't absolute, we need to do more processing to fix paths
-	// such as "../../../../<etc>/some/path". We also shouldn't convert absolute
-	// paths to relative ones.
-	if !filepath.IsAbs(path) {
-		path = filepath.Clean(string(os.PathSeparator) + path)
-		// This can't fail, as (by definition) all paths are relative to root.
-		path, _ = filepath.Rel(string(os.PathSeparator), path)
-	}
-
-	// Clean the path again for good measure.
-	return filepath.Clean(path)
 }
