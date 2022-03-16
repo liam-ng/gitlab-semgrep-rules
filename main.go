42c622efe06e958f29055771e59211d9e6187d91
Daniel Paul Searles <dsearles@gitlab.com>
2022-03-15 21:52:18 -0600

    Update semgrep and deps

--- main.go
+++ main.go
@@ -4,24 +4,14 @@ import (
 	"os"
 
 	log "github.com/sirupsen/logrus"
-	"github.com/urfave/cli/v2"
 
 	"gitlab.com/gitlab-org/security-products/analyzers/command"
-	"gitlab.com/gitlab-org/security-products/analyzers/common/v2/logutil"
 	"gitlab.com/gitlab-org/security-products/analyzers/semgrep/metadata"
 	"gitlab.com/gitlab-org/security-products/analyzers/semgrep/plugin"
 )
 
 func main() {
-	app := cli.NewApp()
-	app.Name = "analyzer"
-	app.Version = metadata.AnalyzerVersion
-	app.Authors = []*cli.Author{{Name: metadata.AnalyzerVendor}}
-	app.Usage = metadata.AnalyzerUsage
-
-	log.SetFormatter(&logutil.Formatter{Project: metadata.AnalyzerName})
-	log.Info(metadata.AnalyzerUsage)
-
+	app := command.NewApp(metadata.AnalyzerDetails)
 	app.Commands = command.NewCommands(command.Config{
 		Match:        plugin.Match,
 		Analyze:      analyze,
